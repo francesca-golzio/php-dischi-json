6da047b41d76bf13c791a0a6987588be5cdddf6c
francesca-golzio <francescagolzio@gmail.com>
2026-07-12 18:47:17 +0200

    feat: print each disc data in page

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-
+  // get (stringified) file content
+  $list = file_get_contents('./discs.json');
+  //var_dump($list);
+  
+  // convert to php associative array
+  $discs = json_decode($list, true);
+  //var_dump($discs);
 
 ?>
 
@@ -11,6 +17,17 @@
   <title>PHP Dischi</title>
 </head>
 <body>
+
+  <?php 
+  // Print each disc data in page
+  foreach($discs as $disc) {
+    echo $disc['title'] . "<br>";
+    echo $disc['artist'] . "<br>";
+    echo $disc['cover_url'] . "<br>";
+    echo $disc['release_year'] . "<br>";
+    echo $disc['genre'] . "<br><br>";
+  }
+?>
 
 </body>
 </html>
