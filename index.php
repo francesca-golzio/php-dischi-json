5d37920b6cfc5aa4ca32df9570fc6a8b2bcb1a26
francesca-golzio <francescagolzio@gmail.com>
2026-07-13 13:12:07 +0200

    feat(ui): add custom style (part_1)

--- index.php
+++ index.php
@@ -12,21 +12,46 @@
 
   <!-- Import Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="src/css/style.css">
 
   <title>PHP Dischi</title>
 </head>
 <body>
 
-  <?php 
-  // Print each disc data in page
-  foreach($discs as $disc) {
-    echo $disc['title'] . "<br>";
-    echo $disc['artist'] . "<br>";
-    echo $disc['cover_url'] . "<br>";
-    echo $disc['release_year'] . "<br>";
-    echo $disc['genre'] . "<br><br>";
-  }
-?>
+<header>
+  <h1>PHP Discs</h1>
+  <h2>Music from the 90's</h2>
+</header>
+
+<main>
+
+  <div class="container">
+    
+    <!-- Print each disc data in page -->
+    <div class="collection">
+
+      <?php 
+        foreach($discs as $disc) {
+          echo "<div class='disc_card card col-8 col-sm-5 col-md-4 col-lg-3 col-xl-2'>
+                  <div class='card-header'>
+                    <h5 class='card-title'>" . $disc['title'] . "</h5>
+                    <h6 class='card-subtitle'>" . $disc['artist'] . "</h6>
+                  </div>
+                  <div class='card-body'>
+                    <img src='" . $disc['cover_url'] . "' class='cover' alt='" . $disc['title'] .  "album cover'>
+                  <div class='info'>
+                      <p class='card-text genre'>" . $disc['genre'] . "</p>
+                      <p class='card-text'>" . $disc['release_year'] . "</p>
+                    </div>
+                  </div>
+                </div>";
+        }
+      ?>
+
+  </div>
+
+</main>
 
 </body>
 </html>
